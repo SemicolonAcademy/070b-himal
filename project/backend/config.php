e50382b1282fcb193c976b12752cfe11e5d75be2
sapkotabhupal <sapkotabhupal@gmail.com>
2013-06-27 15:43:17 +0000

    1. few variables added to config.php 2. links added in navbar.php 3. CRUD code added for subjects section. use this format for all database tables. Two files (main file and respective form.php file). eg. subjects.php & subjects_form.php

--- project/backend/config.php
+++ project/backend/config.php
@@ -1,17 +1,21 @@
 <?php
-	//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+	
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);	
 	mysql_connect("localhost","root","");
 	mysql_select_db("htnepal");
 	
+	$self = $_SERVER['PHP_SELF'];		
+	
+	$self_name = str_replace('.php', '', basename($_SERVER['PHP_SELF'])); // function basename - Returns the trailing file name from given path.
+	
+	//echo basename("sas/123.php/hello.php");	// returns hello.php	
+	//echo basename("sas/123.php/hello.php",".php");	// returns hello without the extension php	
+	
+	$protocol = $_SERVER['HTTPS'] == '' ? 'http://' : 'https://';	
+	$self_url = $protocol . $_SERVER['HTTP_HOST']. $_SERVER['REQUEST_URI']; 	
 	
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	?>
+?>
---
+++ project/backend/subjects_form.php
@@ -0,0 +1,47 @@
+<?php if ($form_action == "edit") { ?>
+	<h3>Edit Subject </h3>                
+<?php } else { ?>
+	<h3>Add Subject</h3>                
+<?php } ?>
+
+<form class="form-horizontal" method ="POST" action="" enctype="multipart/form-data">
+
+
+		<?php if ($form_action == "edit") { ?>
+				<input type="hidden" name="id" value="<?php echo $id;?>">       
+		<?php } ?>
+
+
+		<div class="control-group">
+		  <label class="control-label" for="inputUserId">Name</label>
+		  <div class="controls">
+			<input type="text" name="name" value="<?php if( isset($form_values['name'])) echo $form_values['name']; ?>" />
+			<?php if( isset($errors['name'])){ ?>
+					<span class="help-inline"><?php echo $errors['name'] ;?></span>
+			<?php } ?>
+		  </div>
+		</div>
+		<div class="control-group">
+		  <label class="control-label" for="inputFirstName">Slug</label>
+		  <div class="controls">
+			<input type="text" name="slug" value="<?php if( isset($form_values['slug'])) echo $form_values['slug']; ?>" />
+			<?php if( isset($errors['slug'])){ ?>
+					<span class="help-inline"><?php echo $errors['slug'];?></span>
+			<?php } ?>
+		  </div>
+		</div>
+		<div class="control-group">
+		  <label class="control-label" for="inputPhone">Description</label>
+		  <div class="controls">
+			<textarea rows="8" class="span4" name="description" /><?php if( isset($form_values['description'])) echo $form_values['description']; ?></textarea>				
+		  </div>
+		</div>			
+
+		<div class="control-group">
+		  <div class="controls">
+			<button type="submit" name="submit_<?php echo $form_action;?>" value="submit-<?php echo $form_action;?>" class="btn btn-success">Submit</button>              
+			<a class="btn" href="<?php echo $self; ?>">Cancel</a>			
+			</div>
+		</div>
+		
+</form>
