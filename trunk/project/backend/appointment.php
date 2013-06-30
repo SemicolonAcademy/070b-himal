a070864c2355b781c91e6f7239f2cdbff4b3ab65
cragyioe@gmail.com <cragyioe@gmail.com@88d4a302-7e08-394d-fe31-a93477f16290>
2013-06-30 09:23:23 +0000

    finishing contact and appointment

--- trunk/project/backend/appointment.php
+++ trunk/project/backend/appointment.php
@@ -1,40 +1,134 @@
-<!DOCTYPE html>
 <?php
-     include "config.php";
-     global $ename,$ephone,$eemail,$edate,$euserId; 
-    
-     $time=date("F j, Y, g:i a");
-	      
-	       if(isset($_POST['signin']))
-		    {
-			 if (preg_match("/^[0-9]{1,}$/", $_POST["UserId"]) ==  0)
-			      {$euserId = "   Enter user ID correctly !!";}
-			 if (preg_match("/^[A-Z][a-zA-Z -]+$/", $_POST["Name"]) ==  0)
-			      {$ename = "   Enter name starting with uppercase !!";}
-			 if (preg_match("/^[0-9]{7,10}$/", $_POST["Phone"]) ==  0)
-			      {$ephone = "   Enter seven to ten digit phone number !!";}
-			 if(preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["Email"]) === 0)
-                  {$eemail = " Enter email correctly !!";}
-             if(preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/", $_POST["AppointmentDate"]) === 0)				  
-			      {$edate = " Enter date like (YYYY-MM-DD) !!";}
-			  
-			 if(preg_match("/^[0-9]{1,}$/", $_POST['UserId']) && preg_match("/^[A-Z][a-zA-Z -]+$/", $_POST['Name']) && preg_match("/^[0-9]{7,10}$/", $_POST['Phone']) && preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["Email"]) && preg_match("/^[0-9]{2,4}-[0-9]{1,2}-[0-9]{1,2}$/", $_POST["AppointmentDate"])){
-			     $sql="INSERT INTO `appointments`(`userid`,`name`,`phone`,`email`,`message`,`appointment_date`,`created_at`) VALUES 
-		        ('{$_POST['UserId']}','{$_POST['Name']}','{$_POST['Phone']}','{$_POST['Email']}','{$_POST['Message']}','{$_POST['AppointmentDate']}','{$time}')";
-                 $result=mysql_query($sql);         //send sql line to mysql server to be processed and return the result to result variable.
-              }
+	
+	include "config.php";	
+	$time=date("F j, Y, g:i a");			
+	$table = 'appointments';	
+	$form_action = (isset($_GET['form_action'])) ? $_GET['form_action'] : "insert";		
+	
+	/*-------------------------------------------------
+	Form Validation and Errors
+	-------------------------------------------------*/
+	
+	$errors = array(); 	
+				
+	function form_validation(){
 			
+			 if (preg_match("/^[0-9]{1,}$/", $_POST["userid"]) ==  0){
+					$err['userid'] = "   Enter user ID correctly !!";
+					}
+			 if (preg_match("/^[a-zA-Z -]+$/", $_POST["name"]) ==  0){
+					$err['name'] = "   Enter name correctly !!";
+					}
+			 if (preg_match("/^[0-9]{7,10}$/", $_POST["phone"]) ==  0){
+					$err['phone'] = "   Enter seven to ten digit phone number !!";
+					}
+			 if(preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["email"]) === 0){
+					$err['email'] = " Enter email correctly !!";
+					}
+             if(preg_match("/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/", $_POST["appointment_date"]) === 0){
+					$err['appointment_date']= " Enter date like (YYYY-MM-DD) !!";
+					}
+				return $err;
+	}
+	
+	/*-------------------------------------------------
+	Dalete Data
+	-------------------------------------------------*/
+	
+	if ($form_action == "delete"){		
+		
+			$id=$_GET["id"];							
+			$del="DELETE FROM $table WHERE `id` = $id  LIMIT 1";
+			mysql_query($del);		
+			header("Location: $self");
+	}
+	
+	/*-------------------------------------------------
+	Grab data for single row for edit.
+	-------------------------------------------------*/
+	
+	if ($form_action == "edit" && !isset($_POST['submit_edit'])){
+			
+			$id=$_GET["id"];			
+			
+			$get_single_row_query = "select * from $table WHERE `id` = $id";			
+			$single_row_result = mysql_query($get_single_row_query);			
+			$row_data = mysql_fetch_assoc($single_row_result);
+			$form_values = $row_data;
+	}
+	
+	/*-------------------------------------------------
+	Process Insert Form Submissions
+	-------------------------------------------------*/
+	
+	if (isset($_POST['submit_insert'])){											
+			
+			//$errors  = form_validation();
+			if (! $errors = form_validation()) {									
+			 
+		        $id=$_POST['userid'];
+				$name=$_POST['name'];
+				$phone=$_POST['phone'];
+				$email=$_POST['email'];
+				$message=$_POST['message'];
+				$date=$_POST['appointment_date'];
+				
+				$sql="INSERT INTO `appointments`(`userid`,`name`,`phone`,`email`,`message`,`appointment_date`,`created_at`) VALUES 
+		              ('$id','$name','$phone','$email','$message','$date','$time')";
+				mysql_query($sql) or die(mysql_error());
+				header("location: $self");			
+			
+			} else {
+				$form_values = $_POST;
 			}
-          if(isset($_GET['action']) && $_GET['action'] == 'delete'){
-	        $id=$_GET['id'];
-            $sql="DELETE FROM appointments WHERE id ='{$id}'";		   
-	        $result=mysql_query($sql);
-		    } 
-	 ?>
+		
+	
+	}
+	
+	/*-------------------------------------------------
+	Process Edit Form Submissions
+	-------------------------------------------------*/	
+	
+	if (isset ($_POST['submit_edit'])){												
+						
+			$errors  = form_validation();
+			if (empty($errors)) {
+			
+				$userid=$_POST['userid'];
+				$name=$_POST['name'];
+				$phone=$_POST['phone'];
+				$email=$_POST['email'];
+				$message=$_POST['message'];
+				$date=$_POST['appointment_date'];
+				$time=date("F j, Y, g:i a");					
+	
+				$id = $_POST['id'];										
+				$sql = "UPDATE {$table} SET `userid` = '$userid', `name`= '$name',`phone`= '$phone',`email`='$email',`message`='$message'
+           				,`appointment_date`='$date',`created_at`='$time' WHERE `id` = $id LIMIT 1;";
+				mysql_query($sql) or die(mysql_error());
+				header("location: $self");						
+			} else {
+				$form_values = $_POST;
+			}
+	}
+	
+	
+	/*-------------------------------------------------
+	Grab All Data
+	-------------------------------------------------*/
+		
+	
+	$get_all_query = "select * from $table";
+	$all_result = mysql_query($get_all_query);	
+
+	
+?>
+
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>HTnepal</title>
+    <title>Bootstrap, from Twitter</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -45,6 +139,9 @@
       body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
       }
+	  
+	   .help-inline  {color:#ff0000;}
+	   
     </style>
     <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 
@@ -62,129 +159,56 @@
   </head>
 
   <body>
-    <?php include "navbar.php";?>
-	
+  
+ <?php include "navbar.php";?>
+
     <div class="container">
 
-      <h1>Home Tuition Nepal</h1>
-      <p></p>
-	  
-	  <hr>
-	  <div class="bs-docs-example">
-            <h3>Appointments</h3>
-        
-          </div>
-		  
-		  <form class="bs-docs-example form-horizontal" method ="POST" action="" enctype="multipart/form-data">
-		    <div class="control-group">
-              <label class="control-label" for="inputUserId">User Id</label>
-              <div class="controls">
-                <input type="text" name="UserId" id="inputUserId"  >
-				<?php if($euserId){ ?>
-						<span class="help-inline"><?php echo $euserId ;?></span>
-				<?php } ?>
-              </div>
-            </div>
-            <div class="control-group">
-              <label class="control-label" for="inputFirstName">Name</label>
-              <div class="controls">
-                <input type="text" name="Name" id="inputName" placeholder="John anthon" >
-				<?php if($ename){ ?>
-						<span class="help-inline"><?php echo $ename ;?></span>
-				<?php } ?>
-              </div>
-            </div>
-			<div class="control-group">
-              <label class="control-label" for="inputPhone">Phone</label>
-              <div class="controls">
-                <input type="text" name="Phone" id="inputPhone" >
-				<?php if($ephone){ ?>
-						<span class="help-inline"> <?php echo $ephone ;?>	</span>
-				<?php } ?>
-              </div>
-            </div>
-			<div class="control-group">
-              <label class="control-label" for="inputEmail">Email</label>
-              <div class="controls">
-                <input type="text" name="Email" id="inputEmail">
-				<?php if($eemail){ ?>
-						<span class="help-inline"> <?php echo $eemail ;?>	</span>
-				<?php } ?>
-              </div>
-            </div>
-			<div class="control-group">
-              <label class="control-label" for="inputMessage">Message</label>
-              <div class="controls">
-                <textarea rows="4" name="Message" ></textarea>
-              </div>
-            </div>
-			<div class="control-group">
-              <label class="control-label" for="inputAppointmentDate">Appointment Date</label>
-              <div class="controls">
-               <input type="text"  name="AppointmentDate" id="inputAppointmentDate" placeholder="YYYY-MM-DD" >
-			   <?php if($edate){ ?>
-						<span class="help-inline"> <?php echo $edate ; ?></span>
-				<?php } ?>
-              </div>
-            </div>
-			
-            <div class="control-group">
-              <div class="controls">
-                <button type="submit" name="signin" class="btn btn-success">Sign In</button>              
-				</div>
-            </div>
-          </form>
-		  
-		  <hr size="3" width="100%"color="red">
-		  
-		<?php 
-       //    if(isset($_POST['signin'])){	
-		   $sel="SELECT * FROM appointments";
-		   $result=mysql_query($sel);
-		   if ($row = mysql_fetch_array($result)){
-		   ?>
-		  
-		  <div class="bs-docs-example">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th>ID</th>
-				  <th>User ID</th>
-                  <th>Name</th>
-                  <th>Phone Number</th>
-				  <th>Email</th>
-                  <th>Message</th>
-				  <th>Appointment Date</th>
-				  <th>Created On</th>
-				<!--  <th>Passwod</th>-->
-                </tr>
-              </thead>
-	      <?php
-		   do{ 
-		   ?>
-              <tbody>
-                <tr>
-				  <td><?php echo $row['id'] ?></td>
-                  <td><?php echo $row['userid'] ?></td>				  
-                  <td><?php echo $row['name'] ?></td>
-                  <td><?php echo $row['phone'] ?></td>
-                  <td><?php echo $row['email'] ?></td>
-				  <td><?php echo $row['message'] ?></td>
-				  <td><?php echo $row['appointment_date'] ?></td>
-				  <td><?php echo $row['created_at'] ?></td>
-				  <th><a href="appointment_edit.php?action=edit&id=<?php echo $row['id']; ?>">Edit</a> / <a href="appointment.php?action=delete&id=<?php echo $row['id']; ?> ">Delete</a></th>
-                </tr>
-              </tbody>
-			  <?php }while($row=mysql_fetch_array($result));
-			  }
-			  else{
-			     echo "Sorry ! no records are found." ;
-			  }
-			  ?>
-            </table>
-          </div>
-		  
+	<a href="<?php echo $self_url; ?>"><h3>Appointments</h3></a>
+	
+	<?php if(mysql_num_rows($all_result)) { ?>
+	
+	<table class="table table-hover">
+		<tr>
+			<th>S.N.</th>
+			<th>User Id</th>
+			<th>Name</th>
+			<th>Phone</th>
+			<th>Email</th>
+			<th>Message</th>
+			<th>Appointment Date</th>
+			<th>Created On</th>			
+			<th>Actions</th>
+		</tr>
+		<?php $i=1; while( $single_row = mysql_fetch_assoc($all_result)) { ?>
+			<tr>
+				<td><?php echo $i;?></td>
+				<td><?php echo $single_row['userid']; ?></td>
+				<td><?php echo $single_row['name']; ?></td>
+				<td><?php echo $single_row['phone']; ?></td>
+				<td><?php echo $single_row['email']; ?></td>
+				<td><?php echo $single_row['message']; ?></td>
+                <td><?php echo $single_row['appointment_date']; ?></td>
+                <td><?php echo $single_row['created_at']; ?></td>				
+				<td>
+					<a href="appointment.php?form_action=edit&id=<?php echo $single_row['id'];?>">Edit</a> | 					
+					<a href="appointment.php?form_action=delete&id=<?php echo $single_row ['id'];?>">Delete</a> 
+					
+				</td>
+			</tr>
+		<?php $i++; } ?>
+				
+		</table>
+		
+		<?php } else { ?>
+		Sorry !! no data available.
+		<?php } ?>
+		
+		<hr/>
+		
+		<?php include "appointment_form.php"; ?>
 
+		
     </div> <!-- /container -->
 
     <!-- Le javascript
