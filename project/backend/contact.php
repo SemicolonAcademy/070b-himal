20f6e512bb68bc460dc4b86edb72ac1c22cb04e8
cragyioe <cragyioe@gmail.com>
2013-06-30 09:23:23 +0000

    finishing contact and appointment

--- project/backend/contact.php
+++ project/backend/contact.php
@@ -1,32 +1,89 @@
 <!DOCTYPE html>
 <?php
      include "config.php";
-     global $ename,$ephone,$eemail; 
-    
-     $time=date("F j, Y, g:i a");
-	      
-	       if(isset($_POST['signin']))
-		    {
-			 if (preg_match("/^[A-Z][a-zA-Z -]+$/", $_POST['Name']) ==  0)
-			      {$ename = "   Enter name starting with uppercase !!";}
-			 if (preg_match("/^[0-9]{7,10}$/", $_POST['Phone']) ==  0)
-			      {$ephone = "   Enter seven to ten digit phone number !!";}
-			 if(preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["Email"]) === 0)
-                  {$eemail = " Enter email correctly !!";}			 
-			  
-			 if(preg_match("/^[A-Z][a-zA-Z -]+$/", $_POST['Name']) && preg_match("/^[0-9]{7,10}$/", $_POST['Phone']) && preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["Email"])){
-			     $sql="INSERT INTO `contact`(`name`,`phone`,`email`,`message`,`created_at`) VALUES 
-		        ('{$_POST['Name']}','{$_POST['Phone']}','{$_POST['Email']}','{$_POST['Message']}','{$time}')";
-                 $result=mysql_query($sql);         //send sql line to mysql server to be processed and return the result to result variable.
-              }
+     	
+	 $time=date("F j, Y, g:i a");
+     $table='contact';
+	 
+     $form_action = (isset($_GET['form_action'])) ? $_GET['form_action'] : "insert";
+	 $err=array();
+	 /*------------------------From Validation-------------------------------*/
+	 function validation(){
+			  if (preg_match("/^[a-zA-Z -]+$/", $_POST['Name']) ==  0){
+					$error['name']= "   Enter name properly !!";
+				}
+			 if (preg_match("/^[0-9]{7,10}$/", $_POST['Phone']) ==  0){
+					$error['phone'] = "   Enter seven to ten digit phone number !!";
+				}
+			 if(preg_match("/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/", $_POST["Email"]) === 0){
+					$error['email']=" Enter email correctly !!";
+				}	
+             return $error;
+        }			 
+		
+	 /*------------------------Grab data for edition-------------------------------*/
+	 
+	 if ($form_action == "edit" && !isset($_POST['submit_edit'])){
 			
+			$id=$_GET["id"];			
+
+			$get_single_row_query = "select * from $table WHERE `id` = $id";			
+			$single_row_result = mysql_query($get_single_row_query);			
+			$row_data = mysql_fetch_assoc($single_row_result);
+			$form_values = $row_data;
+	    }
+     
+     /*-----------------------Editing particular contact---------------------*/
+	 
+	  if(isset($_POST['submit_edit'])){
+	        $id=$_POST['id'];
+	        $err  = validation();
+			
+			if (empty($err)) {
+					$sql="UPDATE contact SET `name`='{$_POST['Name']}',`phone`='{$_POST['Phone']}',`email`='{$_POST['Email']}',
+			                        `message`='{$_POST['Message']}',`created_at`='{$time}' WHERE `id`='{$id}'";
+                    mysql_query($sql);
+					header("location: $self");
+                   					
+				}
+				else {
+				$form_values = $_POST;
 			}
-          if(isset($_GET['action']) && $_GET['action'] == 'delete'){
-	        $id=$_GET['id'];
-            $sql="DELETE FROM contact WHERE id ='{$id}'";		   
-	        $result=mysql_query($sql);
-		    } 
-	 ?>
+	  
+	  }	 
+    
+	 /*------------------------Adding contact-------------------------------*/
+     
+	  if(isset($_POST['submit_insert'])){
+		    if(!$err = validation()){
+					 $sql="INSERT INTO `contact`(`name`,`phone`,`email`,`message`,`created_at`) VALUES 
+					      ('{$_POST['Name']}','{$_POST['Phone']}','{$_POST['Email']}','{$_POST['Message']}','{$time}')";
+					 $result=mysql_query($sql);         //send sql line to mysql server to be processed and return the result to result variable.
+					 header("location: $self");	
+			}
+			else {
+				$form_values = $_POST;
+			}
+		}     
+ 	
+     /*-----------------------Deleting particular contact---------------------*/
+	 
+	  if ($form_action == "delete"){		
+		
+			$id=$_GET["id"];							
+			$del="DELETE FROM $table WHERE `id` = $id  LIMIT 1";
+			mysql_query($del);		
+			header("Location: $self");
+	  }
+	 
+     /*-----------------------Deleting particular contact---------------------*/	 
+	 
+	$get_all_query = "select * from $table";
+	$all_result = mysql_query($get_all_query);
+			
+ ?>
+ 
+ 
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -41,6 +98,7 @@
       body {
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
       }
+	  .error  {color:#ff0000;}
     </style>
     <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
 
@@ -62,55 +120,80 @@
 	
     <div class="container">
 
+<<<<<<< .mine
+      	  
+	 
+=======
+>>>>>>> .r52
 	  <div class="bs-docs-example">
+<<<<<<< .mine
+            <a href="<?php echo $self_url; ?>"><h3>Contacts</h3></a> 
+	   <hr>		  
+		<?php 	
+=======
 		<a href="<?php echo $self_url; ?>"><h3>Contacts</h3></a>
         
           </div>
 		  	  
 		<?php 
      	
+>>>>>>> .r52
 		   $sel="SELECT * FROM contact";
 		   $result=mysql_query($sel);
 		   if ($row = mysql_fetch_array($result)){
+		     $i=1;
 		   ?>
 		  
 		  <div class="bs-docs-example">
             <table class="table table-hover">
               <thead>
                 <tr>
-                  <th>ID</th>
+                  <th>S.N.</th>
                   <th>Name</th>
                   <th>Phone Number</th>
 				  <th>Email</th>
                   <th>Message</th>
 				  <th>Created On</th>
+				  <th>Action</th>
 				<!--  <th>Passwod</th>-->
                 </tr>
               </thead>
 	      <?php
-		   do{ 
+		   do{  
 		   ?>
               <tbody>
                 <tr>
-				  <td><?php echo $row['id'] ?></td> 
+				  <td><?php echo $i; ?></td> 
                   <td><?php echo $row['name'] ?></td>
                   <td><?php echo $row['phone'] ?></td>
                   <td><?php echo $row['email'] ?></td>
 				  <td><?php echo $row['message'] ?></td>
 				  <td><?php echo $row['created_at'] ?></td>
-				  <th><a href="contact_edit.php?action=edit&id=<?php echo $row['id']; ?>">Edit</a> / <a href="contact.php?action=delete&id=<?php echo $row['id']; ?> ">Delete</a></th>
+				  <th>
+				      <a href="contact.php?form_action=edit&id=<?php echo $row['id']; ?>">Edit</a> / 
+					  <a href="contact.php?form_action=delete&id=<?php echo $row['id']; ?> ">Delete</a>
+				  </th>
                 </tr>
               </tbody>
-			  <?php }while($row=mysql_fetch_array($result));
+			  <?php $i++;}while($row=mysql_fetch_assoc($result));
 			  }
 			  else{
 			     echo "Sorry ! no records are found." ;
 			  }
 			  ?>
             </table>
+<<<<<<< .mine
 			<hr>
+		  
+		  <?php include"contact_form.php"; ?>
+=======
+			<hr>
+>>>>>>> .r52
           </div>
 		  
+<<<<<<< .mine
+		  
+=======
 		  
 		  <form class="bs-docs-example form-horizontal" method ="POST" action="contact.php" enctype="multipart/form-data">
             <div class="control-group">
@@ -155,6 +238,8 @@
 		  
 	
 	
+>>>>>>> .r52
+
 
     </div> <!-- /container -->
 
