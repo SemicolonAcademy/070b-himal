fe01262325d2fc0ff0275809e3f6f52474720eb3
marutinandanthakur <marutinandanthakur@88d4a302-7e08-394d-fe31-a93477f16290>
2013-07-05 13:13:04 +0000

    assigning HTnepal logo to the title of all pages

--- project/backend/users.php
+++ project/backend/users.php
@@ -257,7 +257,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
       <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
                     <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
+                                    <link rel="shortcut icon" href="assets/ico/logo.png">
                                    <style type="text/css">
 <!--
 .style1 {color: #0000FF}
